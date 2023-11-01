bf1fdd25495236e2a7d57d03e923d3fa2e3dbb29
itamarKovach <ita930174@gmail.com>
2023-11-01 22:24:03 +0200

    ss

--- Top_Words_Counter.py
+++ Top_Words_Counter.py
@@ -7,36 +7,31 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 file_name = sys.argv[1]
-
-def Words_counter(file_name, n):
     # Open the file for reading
-    try:
-        with open(file_name, 'r', encoding='utf-8') as file:
-            text = file.read()
+try:
+     with open(file_name, 'r', encoding='utf-8') as file:
+          text = file.read()
 
-        # Break the text into words and count the frequency of each word
-        words = text.split()
-        word_count = {}
+     # Break the text into words and count the frequency of each word
+     words = text.split()
+     word_count = {}
         
-        for word in words:
-            if word in word_count:
-                word_count[word] += 1
-            else:
-                word_count[word] = 1
-        # sort the words by their freq
+     for word in words:
+         if word in word_count:
+            word_count[word] += 1
+         else:
+            word_count[word] = 1
+         # sort the words by their freq
         
-        most_common_words = sorted(word_count.items(), key = lambda x : x[1], reverse=True)
+     most_common_words = sorted(word_count.items(), key = lambda x : x[1], reverse=True)
 
-        # Show the N most common words
-        for i, (word, count) in enumerate(most_common_words[:N], 1):
-            print(f"{i} - word \"{word}\" {count} times")
-        sys.exit(1)    
-    except FileNotFoundError:
-        print(f"File {file_name} not found.")
-        sys.exit(1)
-
-
-# Get the value N from the user
-N = int(input("Enter the value of N: "))
-Words_counter(file_name,N)
+     # Show the N most common words
+     N = int(input("Enter the value of N: "))
+        
+     for i, (word, count) in enumerate(most_common_words[:N], 1):
+         print(f"{i} - word \"{word}\" {count} times")
+     sys.exit(1)    
+except FileNotFoundError:
+       print(f"File {file_name} not found.")
+       sys.exit(1)
 
