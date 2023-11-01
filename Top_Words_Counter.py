8d54d340348ed4719feeb518cbec3a6a9a46630f
itamarKovach <148196311+itamarKovach@users.noreply.github.com>
2023-11-01 21:59:46 +0200

    changes

--- Top_Words_Counter.py
+++ Top_Words_Counter.py
@@ -1,15 +1,4 @@
 # -*- coding: utf-8 -*-
-
-import sys
-
-# Get the filename as an argument from the command line
-# if len(sys.argv) != 2:
-#     print("Usage: python word_count.py <file_name>")
-#     sys.exit(1)
-
-# file_name = sys.argv[1]
-file_name = "Text.txt"
-
 def Words_counter(file_name, n):
     # Open the file for reading
     try:
@@ -38,6 +27,7 @@ def Words_counter(file_name, n):
 
 if __name__ == "__main__":
     # Get the value N from the user
+        file_name = "Text.txt"
         N = int(input("Enter the value of N: "))
         Words_counter(file_name,N)
 
